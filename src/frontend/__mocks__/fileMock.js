f6118469da4b6a6be187bb6065d612e78887d03a
K-LoZ3 <carlos193zambra@gmail.com>
2021-05-28 09:00:26 -0500

    Configuramos jest para las pruebas unitarias.

---
+++ src/frontend/__mocks__/fileMock.js
@@ -0,0 +1,2 @@
+// Combrobamos que los archivos de imagenes no fallen.
+module.exports = 'test-file-stub';
---
+++ src/frontend/__mocks__/styleMock.js
@@ -0,0 +1,3 @@
+// Con este comprobamos que los estilos no fallan.
+// Estos mocks son para las pruebas unitarias.
+module.expots = {};
